cd9aa701cc94dd7aa413bdfcb2f07902ba964c6d
Yaroslav Ziabko <tiagz@tuta.io>
2021-09-03 10:56:24 +0300

    install:

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 wget https://github.com/santiagz/AutoBackUps/raw/main/backup.sh
 chmod +x backup.sh
 echo "0 16 * * * /root/backup.sh" >> backup
-crontab mycron
-
+crontab backup
+echo "===================================================================="
 echo "Backups will be saved at /var/local "
 echo "thank you for using our services."
